cfa19cc1cfee3b689a103fd42bae9a8e3ab98913
Pohrebniak Andrian <pogra29@gmail.com>
2023-09-22 21:43:31 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -2,5 +2,15 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
 
+const btnChangeColor = document.querySelector('.change-color');
+const spancolor = document.querySelector('.color'); 
+const backcolor = document.querySelector('.widget'); 
+
+const changeBackcolor = () => { 
+ const color = getRandomHexColor(); 
+  spancolor.textContent = color; 
+  backcolor.style.backgroundColor = color; 
+}
+btnChangeColor.addEventListener('click', changeBackcolor); 
