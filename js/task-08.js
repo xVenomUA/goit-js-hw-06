9009aaa5558c0f673294556f163ada2bbc94d304
Pohrebniak Andrian <pogra29@gmail.com>
2023-09-22 21:11:59 +0300

    f

--- js/task-08.js
+++ js/task-08.js
@@ -1,2 +1,17 @@
-const form = document.querySelector('.login-form'); 
-const submitBtn = document.querySelector('.submit-button');
+const form = document.querySelector(".login-form");
+form.setAttribute("autocomplete", "off")
+const personalDate = {};
+const handleclicker = (event) => { 
+    event.preventDefault(); 
+    const form1 = event.currentTarget; 
+    personalDate.email = form1.elements.password.value; 
+    personalDate.password = form1.elements.email.value; 
+    if (personalDate.email === "" || personalDate.password === "") { 
+        alert("Ви повинні все заповнити, інакше вб`ю");
+    } else { 
+        console.log(`Email: ${personalDate.email} \t \t Password: ${personalDate.password}`);
+        form1.reset(); 
+    }
+}
+
+form.addEventListener('submit', handleclicker);
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,4 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
