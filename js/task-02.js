87fee0688bd72b05bafb2b5dafea9685c6e7d047
Pohrebniak Andrian <pogra29@gmail.com>
2023-09-30 17:23:36 +0300

    fix

--- js/task-02.js
+++ js/task-02.js
@@ -6,6 +6,5 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-
 const list = document.getElementById("ingredients");
 list.innerHTML = ingredients.map((element) => `<li>${element}</li>`).join("");
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector('input');
+const createbtn = document.querySelector('button[data-create]');
+const destroybtn = document.querySelector('button[data-destroy]');
+const boxed = document.querySelector('#boxes');
+const destroyBoxes = () => { 
+  boxed.innerHTML = '';
+  console.log("element deleted");
+  alert("Console deleted");
+};
+destroybtn.addEventListener('click', destroyBoxes); 
+const createbnt = (number) => { 
+  const items =[]
+  for (let i = 0; i < number; i++) {
+    const color = getRandomHexColor(); 
+    const item = document.createElement("div");
+    item.dataset.color = color; 
+    item.style.backgroundColor = color;
+    item.style.width = `${30 + i * 10}px`;
+    item.style.height = `30px`;
+    item.style.margin = `10px`; 
+    items.push(item);
+  }
+  boxed.append(...items); 
+}
+createbtn.addEventListener('click', () => createbnt(input.value));
